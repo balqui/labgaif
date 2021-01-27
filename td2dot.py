050ad64a4c38d296e7656f3f68b93517bc6257dc
balqui <jose.luis.balcazar@upc.edu>
2021-01-27 18:16:16 +0100

    first working version, no bells and whistles yet

--- td2dot.py
+++ td2dot.py
@@ -5,9 +5,46 @@ Author: Jose Luis Balcazar, ORCID 0000-0003-4248-4528
 Copyleft: MIT License (https://en.wikipedia.org/wiki/MIT_License)
 
 Construct labeled Gaifman graph of a transactional dataset.
+
+Pending: smarter iterator on .td file to handle comments and such
+
+Don't bother to import graphs from NetworkX, use case is easy enough.
+
+Adjacency lists. 
+
+Graph g is a dict of counters; g maps node u to g[u] which is a 
+Counter of edges: g[u][v] gives how many occurrences we find of 
+the pair (u, v) in a transaction.
+
+Invariant: g[u][v] = g[v][u], careful with the redundancy at output time.
 '''
 
+from collections import Counter, defaultdict
+from itertools import combinations
+
 VERSION = "0.0 alpha"
+
+
+def read_graph_in(filename):
+    '''
+    filename must be a .td file containing only transactions:
+    comments and other variations not supported yet
+    '''
+    gr = defaultdict(Counter)
+    with open(filename) as f:
+        for line in f:
+            transaction = set(line.split())
+            if transaction:
+                for (u,v) in combinations(transaction, 2):
+                    gr[u][v] += 1
+                    gr[v][u] += 1
+    return gr
+
+def dump_graph(gr):
+    for u in gr:
+        for v in gr[u]:
+            if u <= v:
+                print(u, v, gr[u][v], gr[v][u])
 
 
 
@@ -16,7 +53,7 @@ if __name__ == "__main__":
     from argparse import ArgumentParser
     argp = ArgumentParser(
         description = ("Construct dot-coded labeled Gaifman graph" +
-                       " from transactional dataset",
+                       " from transactional dataset"),
         prog = "python[3] td2dot.py or just ./td2dot"
         )
 
@@ -37,12 +74,15 @@ if __name__ == "__main__":
 
     if args.dataset:
         filename = args.dataset
-	else:
-		print("No dataset file specified.")
-		filename = input("Dataset File Name? ")
+    else:
+        print("No dataset file specified.")
+        filename = input("Dataset File Name? ")
 
-	if not filename.endswith(".td"):
-		filename += ".td"
+    if not filename.endswith(".td"):
+        filename += ".td"
+    
+    gr = read_graph_in(filename)
+    dump_graph(gr)
 
 
 
