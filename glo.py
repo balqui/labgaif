db45c00f2a5ea285e08066f1e2fe76f2523a2a7b
balqui <jose.luis.balcazar@upc.edu>
2021-04-28 15:41:52 +0200

    reorganizing MEP s/w

---
+++ glo.py
@@ -0,0 +1,8 @@
+'''
+Author: Jose Luis Balcazar, ORCID 0000-0003-4248-4528 
+Copyleft: MIT License (https://en.wikipedia.org/wiki/MIT_License)
+
+Hack to interface with MEP s/w by gathering together here the global variables.
+'''
+
+TotalAttributesValues = []
