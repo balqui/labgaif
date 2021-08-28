24400bd26de19a63b76835c893961c138814c603
balqui <jose.luis.balcazar@upc.edu>
2021-08-28 17:45:58 +0200

    variations inside pgv_example_2...

--- pgv_example_2_decomp.py
+++ pgv_example_2_decomp.py
@@ -7,7 +7,12 @@ g = pgv.AGraph(compound = "true", directed = "true", newrank = "true")
 g.edge_attr.update(arrowhead = "none")
 g.add_edges_from(zip(range(6), range(1,7)))
 sg = g.subgraph(range(3), "cluster012", rank = "same")
-g.add_edge(1, 8, ltail = "cluster012")
+for nn in sg.iternodes():
+	t = nn
+	break
+if t is None:
+	print ("No node!")
+g.add_edge(t, 8, ltail = "cluster012")
 g.layout("dot")
 g.draw('g.png')
 
